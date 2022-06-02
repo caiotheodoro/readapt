2cdc58b6b4d0b2e02ca146a01b0f3c396ef04e5f
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-06-02 01:26:29 -0300

    fix: 🐛 favicon + meta to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,9 +5,11 @@ export default class Document extends NextDocument {
     render() {
         return (
             <Html>
-                <Head />
                 <title>Readapt</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta name="viewport"  content="width=device-width, initial-scale=1.0" />
+                </Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
