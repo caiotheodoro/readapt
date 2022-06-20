c43e16d4e5558a2c8bcf2129e189d06f52e2d9c3
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-06-19 22:14:24 -0300

    refactor: 💡 image catch comment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,28 +22,28 @@ const Home: NextPage = () => {
 
   const [current, setCurrent] = useState(components[0])
 
-  useEffect(() => {
-    const video = document.createElement("video");
-    video.setAttribute("playsinline", "");
-    video.setAttribute("autoplay", "");
-    video.setAttribute("muted", "");
-    video.style.width = "200px";
-    video.style.height = "200px";
+  // useEffect(() => {
+  //   const video = document.createElement("video");
+  //   video.setAttribute("playsinline", "");
+  //   video.setAttribute("autoplay", "");
+  //   video.setAttribute("muted", "");
+  //   video.style.width = "200px";
+  //   video.style.height = "200px";
 
-    const facingMode = "user";
-    const constraints = {
-      audio: false,
-      video: {
-        facingMode
-      }
-    };
+  //   const facingMode = "user";
+  //   const constraints = {
+  //     audio: false,
+  //     video: {
+  //       facingMode
+  //     }
+  //   };
 
-    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
-      video.srcObject = stream;
-    });
+  //   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+  //     video.srcObject = stream;
+  //   });
 
-    document.body.appendChild(video);
-  }, [])
+  //   document.body.appendChild(video);
+  // }, [])
 
 
   return (
