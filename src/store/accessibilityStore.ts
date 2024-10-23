6dc04adac6093b9bcfca731673582361ef128786
Caio Theodoro <dev.caiotheodoro@gmail.com>
2024-10-23 00:56:06 -0300

    fix: 🐛 accessibility adjusts

--- src/store/accessibilityStore.ts
+++ src/store/accessibilityStore.ts
@@ -17,13 +17,13 @@ interface AccessibilitySettings {
 
 const calculateSettings = (score: number): AccessibilitySettings => {
   if (score >= 7.5) {
-    return { fontSize: 'extra-large', contentDensity: 'minimal', predictedVisualImpairment: 'normal' }
+    return { fontSize: 'extra-large', contentDensity: 'minimal', predictedVisualImpairment: 'High' }
   } else if (score >= 5.5) {
-    return { fontSize: 'very-large', contentDensity: 'very-reduced', predictedVisualImpairment: 'normal' }
+    return { fontSize: 'very-large', contentDensity: 'very-reduced', predictedVisualImpairment: 'Medium' }
   } else if (score >= 4) {
-    return { fontSize: 'large', contentDensity: 'reduced', predictedVisualImpairment: 'normal' }
+    return { fontSize: 'large', contentDensity: 'reduced', predictedVisualImpairment: 'Low' }
   } else {
-    return { fontSize: 'normal', contentDensity: 'normal', predictedVisualImpairment: 'normal' }
+    return { fontSize: 'normal', contentDensity: 'normal', predictedVisualImpairment: 'Low' }
   }
 }
 
@@ -33,7 +33,7 @@ export const useAccessibilityStore = create(
       score: 0,
       fontSize: 'normal',
       contentDensity: 'normal',
-      predictedVisualImpairment: 'normal',
+      predictedVisualImpairment: 'Low',
       setScore: (score: number) => {
         const { fontSize, contentDensity,predictedVisualImpairment } = calculateSettings(score)
         set({ score, fontSize, contentDensity,predictedVisualImpairment })
